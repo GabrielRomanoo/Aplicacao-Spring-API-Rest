6caeff48c60a083e7ffa6a0452979e9c84788217
Gabriel <gabriel.romano.3194@hotmail.com>
2021-02-16 17:39:19 -0300

    Correção de erros

--- src/main/java/br/com/empresa/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/empresa/mvc/mudi/WebSecurityConfig.java
@@ -42,3 +42,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new InMemoryUserDetailsManager(user);
 	}
 }
+
+/* 
+ * Toda a configuração do Spring Security ficou centralizada nesta única classe 
+ * e alguns recursos foram usados para que esta configuração funcionasse. 
+ * 
+ * A primeira é as suas dependências: nada compilaria se não tivéssemos adicionados 
+ * a dependência do módulo de segurança no pom.xml: spring-boot-starter-security.
+ * 
+ * Com isso, nos foi disponibilizado todas as classes necessárias para configurar 
+ * o módulo de segurança: utilizamos um adapter (WebSecurityConfigurerAdapter), 
+ * que vem com várias configurações default. 
+ * Apenas duas destas configurações nós precisamos reimplementar: configure(HttpSecurity) 
+ * e configure(AuthenticationManagerBuilder auth). 
+ * 
+ * Utilizamos uma anotação (@EnableWebSecurity) que disponibiliza todas as dependências 
+ * do Spring Security que precisamos para configurá-lo conforme a necessidade do nosso projeto; 
+ * além disso, ganhamos alguns builders, que nos permitiu criar usuários e configurar a autorização.
+ * 
+ * */
